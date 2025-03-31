e0f0f5c1504834afd0a900b4082f40338a88de0c
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-03-31 12:30:17 -0400

    create entities

---
+++ W25Week12CodeFirstApproach/Standard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W25Week12CodeFirstApproach
+{
+    public class Standard
+    {
+        // scalar properties
+        public int StandardId { get; set; }
+        public string? StandardName { get; set; }
+
+
+    }
+}
---
+++ W25Week12CodeFirstApproach/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W25Week12CodeFirstApproach
+{
+    public class Student
+    {
+        // scalar properties
+        public int StudentId { get; set; }
+        public string? Name { get; set; }
+        public int? StandardId { get; set; }
+    }
+}
